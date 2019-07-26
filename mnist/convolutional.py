c57e1daecf49d52e6857d238110880f103b22dea
32165 <3216561314@qq.com>
2019-07-26 19:49:39 +0800

    修改卷积网络的bug，修改的文件的是convolutonal.py和model.py文件，并完成相依的训练和测试结果

--- mnist/convolutional.py
+++ mnist/convolutional.py
@@ -8,7 +8,7 @@ import input_data
 data = input_data.read_data_sets('MNIST_data',one_hot=True)
 with tf.variable_scope("convolutional"):
     x = tf.placeholder(tf.float32,[None,784],name="x")
-    keep_prob = tf.placeholder(tf)
+    keep_prob = tf.placeholder(tf.float32)
     y,variables = model.convolutional(x,keep_prob)
 
 #训练
@@ -16,33 +16,41 @@ with tf.variable_scope("convolutional"):
 y_ = tf.placeholder(tf.float32,[None,10],name='y')
 #交叉熵
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-#训练补偿
-train_setp = -tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#训练步长，随机梯度下降的方式
+train_setp = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 #预测，判断参数是否相等
 correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
 #正确率
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+#保存
 saver = tf.train.Saver(variables)
 
 with tf.Session() as sess:
+    #合并参数
     merged_summary_op = tf.summary.merge_all()
+    #把参数的路径图
     summay_writer = tf.summary.FileWriter("/tmp/mnist_log/1",sess.graph)
+    #添加图
     summay_writer.add_graph(sess.graph)
     sess.run(tf.global_variables_initializer())
 
+    #训练
     for i in range(20000):
-        batch = data.train_next_batch(50)
+        #训练大小
+        batch = data.train.next_batch(50)
+        #
         if i%100 == 0:
+            #每个一百次，就打印一下准确率
             train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
             print("step %d, training accuracy %g " % (i,train_accuracy))
         sess.run(train_setp,feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
 
+    #测试集
+    print(sess.run(accuracy,feed_dict={x:data.test.images,y_:data.test.labels,keep_prob:1.0}))
 
-print(sess.run(accuracy,feed_dict={x:data.test.images,y_:data.test.labels,keep_prob:1.0}))
+    path = saver.save(
+        sess,os.path.join(os.path.dirname(__file__),'data','convalutional.ckpt'),
+        write_meta_graph = False,write_meta_state = False)
 
-path = saver.save(
-    sess,os.path.join(os.path.dirname(__file__),'data','convalutional.ckpt'),
-    write_meta_graph = False,write_meta_state = False
-)
-
-print("Saved",path)
+    #打印最后保存的路径
+    print("Saved",path)
--- mnist/model.py
+++ mnist/model.py
@@ -12,22 +12,25 @@ def regression(x):
 
     return y,[W,b]
 
-def convolutional(x,keep_prob):
-    # 卷积层
-    def conv2d(x,w):
-        return tf.nn.conv2d([1,1,1,1], padding="SAME")
 
-    # 池化层
+"""定义卷积网络模型"""
+def convolutional(x,keep_prob):
+    # 定义卷积层
+    def conv2d(x,w):
+        return tf.nn.conv2d(x,w,[1,1,1,1], padding="SAME")
+
+    #定义池化层
     def max_pool_2x2(x):
         return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
 
     # 权重
     def weight_variable(shape):
         initial = tf.truncated_normal(shape,stddev=0.1)
+        return tf.Variable(initial)
 
     # 编
-    def bias_variable(shap):
-        inital = tf.constant(0.1,shap=shap)
+    def bias_variable(shape):
+        inital = tf.constant(0.1,shape=shape)
         return tf.Variable(inital)
 
     # 第一层
@@ -44,15 +47,17 @@ def convolutional(x,keep_prob):
     h_pool2 = max_pool_2x2(h_covn2)
 
     #全连接层
-    w_fc1 = weight_variable([7*7*64,1024])
+    w_fc1 = weight_variable([7 * 7 * 64,1024])
     b_fc1 = bias_variable([1024])
-    h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+    h_pool2_flat = tf.reshape(h_pool2,[-1,7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1)+b_fc1)
 
     h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
+    #全连接层
     w_fc2 = weight_variable([1024,10])
     b_fc2 = bias_variable([10])
-    y= tf.nn.softmax(tf.matmul(h_fc1_drop,w_fc2)+b_fc2)
+    y= tf.nn.softmax(tf.matmul(h_fc1_drop,w_fc2) + b_fc2)
 
     return y,[W_conv1,b_conv1,W_conv2,b_conv2,w_fc1,b_fc1,w_fc2,b_fc2]
 
